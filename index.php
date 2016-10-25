63dc6937f6b428cf999eacb54378f378f4d15380
Miguel Castillo <Miguel Castillo>
2016-10-25 12:19:33 -0300

    Se completa codigo de index

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php
-
-	echo "Hola Mundo";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>
+		<?php echo "Hola mundo"; ?>		
+	</h1>
+	<h1>
+		<?php
+			for($i=2; $i<6; $i++)
+				echo "<h$i>¿Como estas? <h$i>\n"; 
+		 ?>
+	</h1>
+</body>
+</html>
